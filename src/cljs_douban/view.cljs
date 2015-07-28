ab9aa35ad07de85f044301cab2f3bbdbf05a5060
Mark Wallace <lotabout@gmail.com>
2015-07-28 22:45:46 +0800

    closure the variable 'current-song' in song-player

--- src/cljs_douban/view.cljs
+++ src/cljs_douban/view.cljs
@@ -13,33 +13,29 @@
        ^{:key (channel "channel_id")}
        [:li (channel "name")])]))
 
-
-(def current-song (atom {}))
-
-(defn song-player-plain []
-  "display songs and controls"
-  (fn []
-    [:div#player
-     [:p "title: " (@current-song "title")]
-     [:audio#player-audio
-      {:autoPlay "true"
-       :controls "true"
-       :src (str (@current-song "url"))}]
-     [:input {:type "button"
-              :value "Next Song"
-              :on-click #(go (reset! current-song (<! (model/skip-song))))}]
-     [:input {:type "button"
-              :value "Play/Stop"
-              :on-click #(let [player (sel1 :#player-audio)]
-                           (if (.-paused player)
-                             (.play player)
-                             (.pause player)))}]]))
-
-;;; react do not support 'onended' event and thus should manually bind it.
-;;; and this is not very 'reagent'-ish
-;;; Also I had to put 'current-song' into global in order to refer to it.
+;;; well, it seems that I can still hide the atom 'current-song'
+;;; why the (with-meat func meta-data) works and not (fn [] ^{meta-data} ;;; body)?
 (def song-player
-  (with-meta song-player-plain
-    {:component-did-mount
-     (fn [this]
-       (dom/listen! (sel1 :#player-audio) :ended #(go (reset! current-song (<! (model/end-song))))))}))
+  "display songs and controls"
+  (let [current-song (atom {})]
+    (with-meta
+      (fn []
+        [:div#player
+         [:p "title: " (@current-song "title")]
+         [:audio#player-audio
+          {:autoPlay "true"
+           :controls "true"
+           :src (str (@current-song "url"))}]
+         [:input {:type "button"
+                  :value "Next Song"
+                  :on-click #(go (reset! current-song (<! (model/skip-song))))}]
+         [:input {:type "button"
+                  :value "Play/Stop"
+                  :on-click #(let [player (sel1 :#player-audio)]
+                               (if (.-paused player)
+                                 (.play player)
+                                 (.pause player)))}]])
+      {:component-did-mount
+       (fn [this]
+         (dom/listen! (sel1 :#player-audio) :ended #(go (reset! current-song (<! (model/end-song))))))})))
+
